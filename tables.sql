e3d6e207d17cb7730c5f6bd3d3de5c3b48addbe6
D-Bits <lockwood.da88@gmail.com>
2025-09-25 16:51:53 -0700

    Sepparted mobile and fixed data into star schemas

--- tables.sql
+++ tables.sql
@@ -1,27 +1,20 @@
 
-CREATE SCHEMA IF NOT EXISTS fcc;
+CREATE SCHEMA IF NOT EXISTS fcc_fixed;
 
-CREATE TABLE IF NOT EXISTS fcc.geo
+CREATE TABLE IF NOT EXISTS fcc_fixed.fact_geo
 (
     id BIGSERIAL,
-    geography_id INT NOT NULL,
+    geography_id VARCHAR NOT NULL,
     geography_type VARCHAR NOT NULL,
     geography_desc VARCHAR NOT NULL,
     total_area VARCHAR,
     PRIMARY KEY(id, geography_id)
 );
 
-CREATE TABLE IF NOT EXISTS fcc.fixed
+CREATE TABLE IF NOT EXISTS fcc_fixed.dim_speed
 (
     id BIGSERIAL,
-    area_data_type VARCHAR,
-    geography_type VARCHAR NOT NULL,
-    geography_desc VARCHAR NOT NULL,
-    geography_id INT NOT NULL,
-    geography_desc_full VARCHAR,
-    total_units INT,
-    biz_res VARCHAR(1),
-    technology VARCHAR,
+    geography_id VARCHAR NOT NULL,
     speed_02_02 DECIMAL,
     speed_10_1 DECIMAL,
     speed_25_3 DECIMAL,
@@ -31,17 +24,58 @@ CREATE TABLE IF NOT EXISTS fcc.fixed
     PRIMARY KEY(id, geography_id)
 );
 
-CREATE TABLE IF NOT EXISTS fcc.mobile
+CREATE TABLE IF NOT EXISTS fcc_fixed.dim_technology
+(
+    id BIGSERIAL,
+    geography_id VARCHAR NOT NULL,
+    area_data_type VARCHAR,
+    biz_res VARCHAR(1),
+    technology VARCHAR,
+    total_units INT,
+    PRIMARY KEY(id, geography_id)
+);
+
+
+CREATE SCHEMA IF NOT EXISTS fcc_mobile;
+
+CREATE TABLE IF NOT EXISTS fcc_mobile.fact_geo
 (
     id BIGSERIAL,
     geography_id INT NOT NULL,
     geography_type VARCHAR NOT NULL,
     geography_desc VARCHAR NOT NULL,
     total_area DECIMAL,
+    PRIMARY KEY(id, geography_id)
+);
+
+CREATE TABLE IF NOT EXISTS fcc_mobile.dim_speed
+(
+    id BIGSERIAL,
+    geography_id INT NOT NULL,
+    speed_5_1 DECIMAL,
+    speed_10_1 DECIMAL,
+    speed_25_3 DECIMAL,
+    speed_100_10 DECIMAL,
+    speed_250_25 DECIMAL,
+    speed_1000_100 DECIMAL,
+    PRIMARY KEY(id, geography_id)
+); 
+
+CREATE TABLE IF NOT EXISTS fcc_mobile.dim_34g
+(
+    id BIGSERIAL,
+    geography_id INT NOT NULL,
     mobilebb_3g_area_st_pct DECIMAL,
     mobilebb_3g_area_iv_pct DECIMAL,
     mobilebb_4g_area_st_pct DECIMAL,
     mobilebb_4g_area_iv_pct DECIMAL,
+    PRIMARY KEY(id, geography_id)
+)
+
+CREATE TABLE IF NOT EXISTS fcc_mobile.dim_5g
+(
+    id BIGSERIAL,
+    geography_id INT NOT NULL,
     mobilebb_5g_spd1_area_st_pct DECIMAL,
     mobilebb_5g_spd1_area_iv_pct DECIMAL,
     mobilebb_5g_spd2_area_st_pct DECIMAL,
