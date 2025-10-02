3b50e3e607a2e2de172b27706324dc790c852690
D-Bits <lockwood.da88@gmail.com>
2025-10-02 15:38:16 -0700

    Removed unused imports.

--- dags/fixed_etl.py
+++ dags/fixed_etl.py
@@ -2,22 +2,17 @@
 DAG to ingest data for fixed broadband.
 """
 from airflow.sdk import dag, task
-from typing import Dict
 import pandas as pd
 import datetime
-import glob
 
 
-@dag(schedule='@daily', start_date=datetime.datetime(2023, 1, 1), catchup=False)
+@dag(schedule='@daily', start_date=datetime.datetime(2025, 1, 1), catchup=False)
 def fixed_etl():
 
     @task()
     def extract() -> dict:
-        
-        # Get all CSV file paths from the directory
-        csv_files = glob.glob('data/fcc/fixed/*.csv')
-        # Read and concatenate all CSV files into a single DataFrame
-        df = pd.concat([pd.read_csv(f, low_memory=False) for f in csv_files], ignore_index=True)
+       
+        df = pd.read_csv("data/fcc/bdc_us_fixed_broadband_summary_by_geography_12-24.csv")
         # Cast the dataframe to a dictionary to share with other tasks in DAG
         df_dict = df.to_dict(orient='records')
 
@@ -36,12 +31,14 @@ def fixed_etl():
     @task()
     def load(df_dict: dict):
 
+        df = pd.DataFrame(df_dict)
         # Here you would typically load the DataFrame to a database or data warehouse
         # For demonstration, we will just print the DataFrame shape
         print(f"Data loaded with shape: {df.shape}")    
 
     extracted_data = extract()
     transformed_data = transform(extracted_data)
+    load(transformed_data)
     
 
 fixed_etl()
