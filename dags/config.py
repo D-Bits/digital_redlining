ee26c4d35fdf6bd0eda1a8a1ae75e783f92e801a
D-Bits <lockwood.da88@gmail.com>
2025-10-07 16:58:00 -0700

    Added configs.

--- dags/config.py
+++ dags/config.py
@@ -1,9 +1,14 @@
 """
 Config file for various DAG settings.
 """
+import sqlalchemy
 import os 
 
 
 # Environment variables
 AIRFLOW_DB_CONN = os.getenv("AIRFLOW_DB_CONN")
 REDLINING_DB_CONN = os.getenv("REDLINING_DB_CONN")
+
+# DB SQL Alchemy engines
+redlining_engine = sqlalchemy.create_engine(REDLINING_DB_CONN)
+airflow_engine = sqlalchemy.create_engine(AIRFLOW_DB_CONN)
