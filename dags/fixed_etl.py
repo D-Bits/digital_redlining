8a3f99071d588d1302f42c69256cc78bbdab5524
D-Bits <lockwood.da88@gmail.com>
2025-10-09 13:49:17 -0700

    Data transformations.

--- dags/fixed_etl.py
+++ dags/fixed_etl.py
@@ -29,7 +29,36 @@ def fixed_etl():
     def transform(df_dict: dict) -> dict:
 
         df = pd.DataFrame(df_dict)
-        df_dict = df.to_dict(orient='records')
+
+        fact_geo = df[[
+            "geography_id",
+            "area_data_type", 
+            "geography_type", 
+            "geography_desc", 
+            "total_area"
+        ]]
+        dim_speed = df[[
+            "geography_id", 
+            "speed_02_02", 
+            "speed_10_1", 
+            "speed_25_3", 
+            "speed_100_20", 
+            "speed_250_25", 
+            "speed_1000_100"
+        ]]
+        dim_tech = df[[
+            "geography_id", 
+            "area_data_type",
+            "biz_res",
+            "technology",
+            "total_units"
+        ]]
+
+        df_dict = {
+            "fact_geo": fact_geo.to_dict(orient='records'),
+            "dim_speed": dim_speed.to_dict(orient='records'),
+            "dim_tech": dim_tech.to_dict(orient='records')
+        }
 
         return df_dict
         
@@ -37,10 +66,16 @@ def fixed_etl():
     @task()
     def load(df_dict: dict):
 
-        df = pd.DataFrame(df_dict)
-        # Here you would typically load the DataFrame to a database or data warehouse
-        # For demonstration, we will just print the DataFrame shape
-        print(f"Data loaded with shape: {df.shape}")    
+        fact_geo = pd.DataFrame.from_dict(df_dict["fact_geo"])
+        dim_speed = pd.DataFrame.from_dict(df_dict["dim_speed"])
+        dim_tech = pd.DataFrame.from_dict(df_dict["dim_tech"])
+
+        
+
+        print(f"Data loaded into fact_geo with shape: {fact_geo.shape}")
+        print(f"Data loaded into fact_geo with shape: {dim_speed.shape}")    
+        print(f"Data loaded into fact_geo with shape: {dim_tech.shape}")    
+
 
     extracted_data = extract()
     transformed_data = transform(extracted_data)
